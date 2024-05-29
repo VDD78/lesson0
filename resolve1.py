e882cfb53e077f6c98aa4503d4795a4a94b6eaa1
VDD78 <vdd78@bk.ru>
2024-05-29 21:41:53 +0700

    Add files via upload

---
+++ resolve1.py
@@ -0,0 +1,23 @@
+a='МАРС'
+aa='В слове -'
+b=len(a)
+bb='буквы'
+print(aa,a,'-',b,bb)
+first=3
+second=4
+sum=first+second
+diff=first-second
+print(sum,diff)
+first=5
+second=5
+third=10
+mean=(first+second+third)/3
+print(mean)
+first_string='Понедельник'
+second_string='Вторник'
+print(first_string+', '+second_string)
+a=2
+b=3
+c=4
+f=(((a*b)+(a*c))**3)/2
+print(f)
