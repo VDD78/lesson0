62a0822c3a2320bf87d416cd19a2437b9b4ab4bf
VDD78 <vdd78@bk.ru>
2024-06-01 10:22:22 +0700

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -7,3 +7,12 @@ f='среднее время выполнения'
 g=(e/c)
 j='часа'
 print(a,b,c,',',d,e,',',f,g,j)
+name='Python,'
+first_data='всего задач'
+first_task=12
+second_data='затрачено часов'
+second_task=1.5
+third_data='среднее время выполнения'
+third_task=(second_task/first_task)
+fourth_data='часов'
+print(name,first_data, first_task,',',second_data,second_task,',',third_data,third_task, fourth_data)
